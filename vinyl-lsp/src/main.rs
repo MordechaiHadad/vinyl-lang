03a9a1e40f837c162c55c43a6c19bda762173ed4
MordechaiHadad <mordechai.hadad01@gmail.com>
2026-07-28 13:37:25 +0300

    updated env filter for tracing

--- vinyl-lsp/src/main.rs
+++ vinyl-lsp/src/main.rs
@@ -34,7 +34,7 @@ fn init_tracing(verbose: u8) -> Result<()> {
             _ => EnvFilter::new("trace"),
         }
     } else {
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("warn"))
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"))
     };
     tracing_subscriber::registry()
         .with(filter)
